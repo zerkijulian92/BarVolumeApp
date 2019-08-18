cf89ea7dfbe30dfd1e9a7ca0ca806af90860dd6b
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-18 11:50:51 +0700

    Menambahkan metode Onclick di kelas MainActivity.java

--- app/src/main/java/com/takatutustudio/barvolumeapp/MainActivity.java
+++ app/src/main/java/com/takatutustudio/barvolumeapp/MainActivity.java
@@ -2,13 +2,14 @@ package com.takatutustudio.barvolumeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText edtWidth, edtHeight, edtLenght;
+    EditText edtWidth, edtHeight, edtLength;
     Button btnCalculate;
     TextView tvResult;
 
@@ -19,13 +20,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         edtWidth = findViewById(R.id.edt_width);
         edtHeight = findViewById(R.id.edt_height);
-        edtLenght = findViewById(R.id.edt_lenght);
+        edtLength = findViewById(R.id.edt_lenght);
         btnCalculate = findViewById(R.id.btn_calculate);
         tvResult = findViewById(R.id.tv_result);
+
+        //Button Calculate / Hasil ketika di klik
+        btnCalculate.setOnClickListener(this);
     }
 
+    //Fungsi dari btnCalculate
     @Override
     public void onClick(View v) {
-        
+        if (v.getId() == R.id.btn_calculate) {
+            String inputLenght = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            //Pesan error ketika Edit Text Kosong tidak di isi oleh user
+
+            if (TextUtils.isEmpty(inputLenght)) {
+                isEmptyFields = true;
+                edtLength.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true;
+                edtWidth.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak boleh kosong");
+            }
+
+            //-------------
+
+            Double length = toDouble(inputLenght);
+            Double width = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+            if (length == null) {
+                isInvalidDouble = true;
+                edtLength.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (width == null) {
+                isInvalidDouble = true;
+                edtWidth.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (height == null) {
+                isInvalidDouble = true;
+                edtHeight.setError("Field ini harus berupa nomer yang valid");
+            }
+
+           //Melakukan action ketika data sudah di inputkan dan tidak mengalami error
+            if ( !isEmptyFields && !isInvalidDouble) {
+                double volume = length * width * height;
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+
+    }
+
+    Double toDouble(String str) {
+        //tulis disini
     }
 }

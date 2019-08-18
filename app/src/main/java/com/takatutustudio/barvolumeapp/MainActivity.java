09e166c5a1d1e5ae8d3db4abcb52cd941d08d050
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-08-18 10:56:25 +0700

    Mendeklarasikan ID di file MainActivity.java

--- app/src/main/java/com/takatutustudio/barvolumeapp/MainActivity.java
+++ app/src/main/java/com/takatutustudio/barvolumeapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.takatutustudio.barvolumeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtWidth, edtHeight, edtLenght;
+    Button btnCalculate;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLenght = findViewById(R.id.edt_lenght);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
     }
 }
